05b9f4cca32e7081257105a34b4cea6586925c1e
hjh978 <3329993270@qq.com>
2025-09-27 21:18:36 +0800

    init: 项目结构初始化

---
+++ qk-management/src/main/java/com/qk/management/QkManagementApplication.java
@@ -0,0 +1,16 @@
+package com.qk.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: hjh
+ * @Date: 2025/09/27 20:43
+ * @Description: 轻客管家项目启动类
+ */
+@SpringBootApplication
+public class QkManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QkManagementApplication.class, args);
+    }
+}
