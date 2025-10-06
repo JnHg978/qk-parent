3b16c8a37099e0dc0174a432063465fdc9fdc73b
hjh978 <3329993270@qq.com>
2025-10-06 19:48:33 +0800

    feat: 全局异常处理

---
+++ qk-management/src/main/java/com/qk/management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.qk.management.exception;
+
+import com.qk.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    //处理异常
+    @ExceptionHandler
+    public Result ex(Exception e){//方法形参中指定能够处理的异常类型
+        log.error("服务器发生异常", e);
+        //捕获到异常之后，响应一个标准的Result
+        return Result.error("对不起,操作失败,请联系管理员");
+    }
+}
