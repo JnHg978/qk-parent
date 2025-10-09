402cd3a4316fa01ae1b5ca0d14cc2db071dc7277
hjh978 <3329993270@qq.com>
2025-10-09 15:25:01 +0800

    feat: 添加过滤器Filter校验token

--- qk-management/src/main/java/com/qk/management/QkManagementApplication.java
+++ qk-management/src/main/java/com/qk/management/QkManagementApplication.java
@@ -2,12 +2,14 @@ package com.qk.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @Author: hjh
  * @Date: 2025/09/27 20:43
  * @Description: 轻客管家项目启动类
  */
+@ServletComponentScan
 @SpringBootApplication
 public class QkManagementApplication {
     public static void main(String[] args) {
---
+++ qk-management/src/main/java/com/qk/management/filter/TokenFilter.java
@@ -0,0 +1,55 @@
+package com.qk.management.filter;
+
+import com.qk.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+/**
+ * @Author: hjh
+ * @Date: 2025/10/09 14:36
+ * @Description:
+ */
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class TokenFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("开始过滤请求......");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        if ("/login".equals(httpServletRequest.getRequestURI())) {
+            log.info("登录操作, 直接放行");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = httpServletRequest.getHeader("token");
+        if (token == null) {
+            log.info("解析 token 出错, 拒绝访问, 401");
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        try {
+            // 解析 token
+            Claims claims = JwtUtils.parseToken(token);
+        } catch (Exception e) {
+            // 捕获解析 token 异常，返回 401 错误
+            log.info("解析 token 出错, 拒绝访问, 401");
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        // 继续执行后续过滤器或请求处理
+        log.info("token 正确, 继续执行后续过滤器或请求处理");
+        chain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+}
